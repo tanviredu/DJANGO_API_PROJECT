fa04498d6ce348361c6eff59fed9ffa0a71148d4
mechanic <ornobtanvir.git@gmail.com>
2020-01-21 13:07:48 +0600

    added

--- profile_project/profile_API/models.py
+++ profile_project/profile_API/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+## this two packages are needed 
+## for overwriting the django default user model
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+## user profile models
+## we overwrite the django default user models
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """ database model for user in the system """
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    ## this is the custom user manager
+    ## so the django admin understand and interact how to deal with the new class
+    ## specfy the model manager
+    objects = UserProfileManager()
+
+    ## we overwrite the default username field with emal
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+
